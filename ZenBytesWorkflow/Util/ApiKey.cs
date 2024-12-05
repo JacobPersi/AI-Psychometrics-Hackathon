28b0ec7360aa29b93179149d74f9cfcf6f368ce6
Jacob Persi <me@JacobPersi.com>
2024-12-05 11:24:06 -0500

    API key file doesn't work due to web project. Whatever.

--- ZenBytesWorkflow/Util/ApiKey.cs
+++ ZenBytesWorkflow/Util/ApiKey.cs
@@ -1,27 +1,6 @@
 ﻿namespace ZenBytesWorkflow.Util;
 
-using System.IO;
-
 public static class ApiKey
 {
-	private static string _cachedApiKey;
-	private static string _filePath = "api_key.txt";
-
-	public static string Value
-	{
-		get
-		{
-			if (_cachedApiKey == null)
-			{
-				if (!File.Exists(_filePath))
-				{
-					throw new FileNotFoundException("The API key file was not found.", _filePath);
-				}
-
-				_cachedApiKey = File.ReadAllText(_filePath).Trim();
-			}
-
-			return _cachedApiKey;
-		}
-	}
+	public static string Value = "AIzaSyDlRqMYEvnEx9SJjoc0mJqn8uDvJCLet7I";
 }
